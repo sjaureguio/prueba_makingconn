319df49d8234e11729e2b52bc16139ef1124eb5f
Solano Jauregui <solanojauregui98@gmail.com>
2022-10-26 17:42:02 -0500

    Crea controlador de cliente

---
+++ app/Http/Controllers/ClientesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\ClienteRequest;
+use App\Http\Resources\ClientesCollection;
+use App\Http\Resources\ClienteResource;
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClientesController extends Controller
+{
+    
+}
